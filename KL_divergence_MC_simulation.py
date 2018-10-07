dcb311d3f8fd11e9b1a0a1ecab340a3ec5ac0304
Victor <31482851+VictorZuanazzi@users.noreply.github.com>
2018-10-07 14:09:30 +0200

    rearanged some code

--- KL_divergence_MC_simulation.py
+++ KL_divergence_MC_simulation.py
@@ -29,10 +29,9 @@ def plot_2_normal_distributions(means, stds):
     
 def MC_KL_divergence(p1, p2):
     assert len(p1) == len(p2)
-    n = len(p1)
+    n = len(p2)
     
     return (np.log(p1/p2)).sum()/n
-
 
 
 if __name__ == '__main__':
@@ -43,29 +42,30 @@ if __name__ == '__main__':
     stds = sigma, tau
     plot_2_normal_distributions(means, stds)
     
+    theoretical_fg = (np.log(tau**2) -np.log(sigma**2) -1 + (sigma**2 +(mu - nu)**2)/(tau**2))/2
+    theoretical_gf = (np.log(sigma**2) -np.log(tau**2) -1 + (tau**2 +(mu - nu)**2)/(sigma**2))/2
+    
+    print("Theoretical KL(f||g) \t= {0} Theoretical KL(f||g) \t= {1}".format(theoretical_fg, theoretical_gf))
+    print("Sample Size | Monte Carlo KL(f||g) | Monte Carlo KL(g||f)")
     for size in [10, 1000, 10000, 1000000]:
-        print 'size: ',size
+        #print 'size: ',size
         # draw n samples from p(x)
         x = np.random.normal(size = size, loc = mu, scale = sigma)
         f = norm.pdf(x, loc = mu, scale = sigma)
         g = norm.pdf(x, loc = nu, scale = tau)
-        #print('x: ', x)
-        #print('f: ', f)
-        #print('g: ', g)
         
-        theoretical_fg = (np.log(tau**2/sigma**2) -1 + (sigma**2 +(mu - nu)**2)/(tau**2))/2
         mc_fg = MC_KL_divergence(f, g)
         
-        print("Theoretical KL(f || g) \t= {0}\nMonte Carlo KL(f || g) \t= {1}".format(theoretical_fg, mc_fg))
+        #print("Theoretical KL(f || g) \t= {0}\nMonte Carlo KL(f || g) \t= {1}".format(theoretical_fg, mc_fg))
         # draw n samples from q(x)
         x = np.random.normal(size = size, loc = nu, scale = tau)
         f = norm.pdf(x, loc = mu, scale = sigma)
         g = norm.pdf(x, loc = nu, scale = tau)
         
-        theoretical_gf = (np.log(sigma**2) - np.log(tau**2) -1 + (tau**2 +(mu - nu)**2) / (sigma**2))/2
+        
         mc_gf = MC_KL_divergence(g, f)
         
-        print("Theoretical KL(g || f) \t= {0}\nMonte Carlo KL(g || f) \t= {1}".format(theoretical_gf, mc_gf))
+        print size, ' |', mc_fg, ' |', mc_gf
         # sanity check
-        print( MC_KL_divergence(f, f) == MC_KL_divergence(g, g) ==0) 
+        #print( MC_KL_divergence(f, f) == MC_KL_divergence(g, g) ==0) 
 
